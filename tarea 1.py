51d8c13968817b4b1d2101b4cab4d1c5cbfdb933
¨Julio <julio.bernal6369@alumnos.udg.mx>
2026-09-14 21:18:34 -0600

    Agregar conteo

--- tarea 1.py
+++ tarea 1.py
@@ -1,7 +1,12 @@
 print("Análisis estadístico de datos")
 datos = [10, 20, 30, 40, 50]
 print("Datos:", datos)
-suma = sum(datos)
-print("Suma:", suma)
-promedio = sum(datos) / len(datos)
-print("Promedio:", promedio)
+
+    suma = sum(datos)
+    print("Suma:", suma)
+
+    promedio = sum(datos) / len(datos)
+    print("Promedio:", promedio)
+
+    cantidad = len(datos)
+    print("Cantidad de datos:", cantidad)
